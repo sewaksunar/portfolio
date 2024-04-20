8552b56a8d9c739585c91b908f715358cfd4cfaf
sewaksunar <080bme051@ioepc.edu.np>
2024-04-20 13:59:43 +0545

    preloader added

---
+++ JScript.js
@@ -0,0 +1,7 @@
+<script>
+    // Hide preloader when the page is fully loaded
+    window.addEventListener("load", function () {
+        var preloader = document.querySelector(".preloader-container");
+        preloader.style.display = "none";
+    });
+</script>
